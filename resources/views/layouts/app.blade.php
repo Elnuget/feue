ea1aa5cc2d07a5c7177eb9f3d1d0420b3e81b805
RichardDebugFile <123910016+RichardDebugFile@users.noreply.github.com>
2024-12-16 22:05:33 -0500

    Proceso para cambio de "Title"

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Cap U 🎓 @hasSection('page_title') - @yield('page_title') @endif</title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
--- resources/views/matriculas/index.blade.php
+++ resources/views/matriculas/index.blade.php
@@ -1,4 +1,5 @@
 <x-app-layout>
+    @section('page_title', 'Matrículas')
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Matriculas') }}
