fe4917387b84c25cc4f26240eec7e4c87854472e
RichardDebugFile <123910016+RichardDebugFile@users.noreply.github.com>
2024-12-17 22:08:44 -0500

    Pruebas de Controladores

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,18 +1,24 @@
 <x-guest-layout>
+    @section('page_title', 'Register')
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
         <!-- Nombre -->
         <div>
             <x-input-label for="name" :value="__('Nombre')"/> 📝
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <!-- Aplicamos text-transform: uppercase para mostrar el texto en mayúsculas -->
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" 
+                          :value="old('name')" required autofocus autocomplete="name" 
+                          style="text-transform: uppercase;"/>
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Correo Electrónico -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Correo Electrónico')"/> 📧
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full" 
+                          type="email" name="email" 
+                          :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -20,9 +26,9 @@
         <div class="mt-4">
             <x-input-label for="password" :value="__('Contraseña')"/> 🔒
             <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+                          type="password"
+                          name="password"
+                          required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
@@ -30,8 +36,8 @@
         <div class="mt-4">
             <x-input-label for="password_confirmation" :value="__('Confirmar Contraseña')"/> 🔒
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
+                          type="password"
+                          name="password_confirmation" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Cap U 🎓 @hasSection('page_title') - @yield('page_title') @endif</title>
+        <!-- Favicon -->
+        <link rel="icon" href="{{ asset('favicon.png') }}" type="image/png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -14,6 +16,7 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
+
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
             <div>
